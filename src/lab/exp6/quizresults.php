7c1379a071e37e0395ad5d67e2211fc442033bc3
sridhar2k1 <sridharvanga2001@gmail.com>
2019-03-28 21:20:07 +0530

    fixed in quizresults.php

--- src/lab/exp6/quizresults.php
+++ src/lab/exp6/quizresults.php
@@ -64,17 +64,17 @@ echo "	<h2>Correct Answers</h2>
 		<li><b>For every recursive solution, there is a corresponding iterative solution</b></li>
 		True<br>
 		<br>
-		
+
 		<li><b>What will be the ouptut of the following function call: fun2(20); where fun2 is defined as: </b> </li>
 		20 40 60 80 80 60 40 20<br>
-		<br>	
+		<br>
 
 		<li><b>What will be the ouptut of the following function call: fun3(100); where fun3 is defined as: </b> </li>
 		6<br>
 		<br>
 
 		 <li><b>What will be the ouptut of the following function call: fun4(4,3); where fun4 is defined as: </b> </li>
-		 14<br>
+		 13<br>
 		 <li><b>A recursive function must necessarily make a call to itself. </b> </li>
 		 <li><b>A base case (or end-condition) is required so that there is no infinite recursive calling of the same function.</b> </li>
 		 <br>
